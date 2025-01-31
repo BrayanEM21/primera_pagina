7d176f03e898acceb15ff13437217fd6cbf848b9
Brayan Espinosa <brayanespinosa915@gmail.com>
2025-01-30 18:39:21 -0600

    funcion de imagenes

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function mostrarImagen(){
-    let imagen = document.getElementById("imagenes");
-    imagenes.classList.toggle("hideen");
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const imagenes = document.querySelectorAll("#imagenes img");
+    let index = 0;
+
+    function cambiarImagen() {
+       
+        imagenes.forEach((img) => img.classList.remove("visible"));
+
+        imagenes[index].classList.add("visible");
+
+    
+        index = (index + 1) % imagenes.length;
+    }
+
+    setInterval(cambiarImagen, 3000);
+
+    cambiarImagen();
+});
